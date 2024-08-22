8f11210f8198bcc2cea2ead126ee395ad4636549
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-22 13:50:48 -0500

    chore: Add the first endpoint with flask  and a basic route for /taxis and methods GET. Also, it is tested in Postman #4

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__) # Crea una instancia de la clase Flask
+
+@app.route('/taxis', methods=['GET'])  # Usa route() para definir la URL que activará la función y GET para definir el método HTTP
+def hello_world():
+    return '<p>Hello, World!</p>'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000) # run the application
