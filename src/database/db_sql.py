1da94177a6bc4e4f9f05e5f61b19b9572ca842f5
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-27 12:54:48 -0500

    feat: update of connection to database with class sessionmaker #6

--- src/database/db_sql.py
+++ src/database/db_sql.py
@@ -1,19 +1,25 @@
 # conected to the database and execute the sql query
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
 from dotenv import load_dotenv
 import os
 
-def get_connection():
-    
+# load of variable of environment
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
+
+# create of a new database connection - motor 
+engine = create_engine(database_url) 
+
+# class of SQLAlchemy that create a new instance of session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_session():
     try:
-        load_dotenv() #variable of environment
-        database_url = os.getenv("DATABASE_URL")
-        engine = create_engine(database_url) # create_engine: is used to create a new database connection
-        connection = engine.connect() # engine.connect: is used to connect to the database
-        print("Connected to database")
-        return connection # return the connection
+        return SessionLocal() # return of instance session
     
     except Exception as e:
         print("Error al conectar con la base de datos ", e)
+        return None
     
