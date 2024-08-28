8ad957e236f8bb80f968f09a1f732abb3eecf081
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-28 16:30:08 -0500

    chore: update de values of default of query params #7 #9

--- app/controllers/taxis_controller.py
+++ app/controllers/taxis_controller.py
@@ -3,8 +3,8 @@ from app.services.taxis_services import fetch_taxis
 
 def get_taxis():
     plate = request.args.get('plate', type=str, default=None)
-    page = request.args.get('page', type=int, default=1)
-    limit = request.args.get('limit', type=int, default=10)
+    page = request.args.get('page', type=int, default=0)
+    limit = request.args.get('limit', type=int, default=0)
     
     if page < 0 or limit < 0:
         return jsonify({"error": "Page number and limit must be greater than 0."}), 400
