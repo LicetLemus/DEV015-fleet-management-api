88f139d2474c67b02da3b6e2838deded269978ce
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-30 17:32:13 -0500

    refactor: refactor endpoint /trajectories #20

--- app/controllers/trajectories_controller.py
+++ app/controllers/trajectories_controller.py
@@ -4,34 +4,28 @@ from app.services.trajectories_services import fetch_trajectories
 
 def get_trajectories():
     """
-    Gets all trajectories for a specific taxi on a date range in format JSON.
+    Gets all trajectories for a specific taxi within a date range.
 
     Query Parameters:
-    - taxi_id: ID of the taxi (string).
-    - date: Date in format dd-mm-yyyy.
-
-    Responses:
-    - 200 OK: Returns a JSON list of trajectories.
-    - 400 Bad Request: If taxi_id or date is missing, or if the date format is incorrect.
-    - 404 Not Found: If no trajectories are found for the given taxi and date.
-    - 500 Internal Server Error: In case of server error.
+    - taxi_id (str): ID of the taxi.
+    - date (str): Date in 'dd-mm-yyyy' format.
 
     Returns:
-    - Flask Response: JSON with the result of the query or an error message.
+    - Flask Response: JSON with the list of trajectories or an error message.
     """
-    
     print("------------------------------- get_trajectories") 
-    taxi_id = request.args.get('taxi_id', type=str)
-    date_str = request.args.get('date', type=str)
     
-    # Check if exist taxi_id and date
-    if not taxi_id or not date_str:
-        return jsonify({"error": "taxi_id and date are required"}), 400
+    taxi_id, date_str = get_query_params_trajectories()
+    
+    validation_result = validation_query_params_trajectories(taxi_id, date_str)
+    if validation_result:
+        return validation_result
     
     date_initial_str, date_end_str = parse_date(date_str)
+    if not date_initial_str or not date_end_str:
+        return jsonify({"error": "Date must be in 'dd-mm-yyyy' format."}), 400
 
     try:
-        # Fetch trajectories from the database using the given taxi_id and date range
         result_query = fetch_trajectories(taxi_id, date_initial_str, date_end_str)
         
         if 'error' in result_query:
@@ -42,9 +36,47 @@ def get_trajectories():
     except Exception as e:
         # Handle any exceptions that occur during the process
         return jsonify({"Error": str(e)}), 500
+
+
+def get_query_params_trajectories():
+    """
+    Retrieve query parameters for taxi ID and date from the request.
+
+    Returns:
+    - (taxi_id, date_str)
+    """
+    taxi_id = request.args.get('taxi_id', type=str)
+    date_str = request.args.get('date', type=str)
     
+    return taxi_id, date_str
+
+
+def validation_query_params_trajectories(taxi_id, date_str):
+    """
+    Validate taxi_id and date_str parameters.
+
+    Args:
+    - taxi_id (str): ID of the taxi.
+    - date_str (str): Date in 'dd-mm-yyyy' format.
+
+    Returns:
+    - Flask Response: JSON with an error message and a 400 status code if validation fails, otherwise None.
+    """
+    if not taxi_id or not date_str:
+        return jsonify({"error": "taxi_id and date are required"}), 400
+
 
 def parse_date(date_str):
+    """ 
+    Parse the date string and return the start and end of the day in 'YYYY-MM-DD HH:MM:SS' format.
+
+    Args:
+    - date_str (str): Date in 'dd-mm-yyyy' format.
+
+    Return:
+    - (start_of_day, end_of_day) in 'YYYY-MM-DD HH:MM:SS' format.
+    """
+
     try:
         date_obj = datetime.strptime(date_str, '%d-%m-%Y')
         date_initial = date_obj.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -52,3 +84,4 @@ def parse_date(date_str):
         return date_initial.strftime('%Y-%m-%d %H:%M:%S'), date_end.strftime('%Y-%m-%d %H:%M:%S')
     except ValueError:
         return None, None
+    
--- app/services/trajectories_services.py
+++ app/services/trajectories_services.py
@@ -9,50 +9,34 @@ def fetch_trajectories(taxi_id, date_initial, date_end):
 
     Parameters:
     - taxi_id: ID of the taxi (string).
-    - date_start: Start date of the range in 'YYYY-MM-DD HH:MM:SS' format.
+    - date_initial: Start date of the range in 'YYYY-MM-DD HH:MM:SS' format.
     - date_end: End date of the range in 'YYYY-MM-DD HH:MM:SS' format.
 
     Returns:
     - List of dictionaries with trajectory information if results are found.
     - Dictionary with an error message if no trajectories are found or if there is a problem.
-
-    Exceptions:
-    - Raises an exception if there is an error connecting to the database or during the query.
-
-    Returns:
-    - List or dict: List of trajectories or dictionary with an error message.
     """
     
     print("------------------------------- fetch_trajectories")
-    print(taxi_id, date_initial, date_end)
     session = get_session()
     validation_session(session)
     
     try:
         # Start building the query
-        query = session.query(Trajectories)
-
-        if taxi_id and date_initial and date_end:
-            # Filter results based on taxi_id and date range
-            query = query.filter(
-                Trajectories.taxi_id == taxi_id,
-                Trajectories.date >= date_initial,
-                Trajectories.date <= date_end 
-            )
+        result_query = session.query(Trajectories)
+        result_query = get_query_with_params_trajectories(result_query, taxi_id, date_initial, date_end)
             
         # Prepare to collect the results
-        trajectories_list = []
-
-        for row in query:
-            trajectories_list.append(
-                {
+        trajectories_list = [
+            {
                 "id": row.id,
                 "taxi_id": row.taxi_id,
                 "date": row.date,
                 "latitude": row.latitude,
                 "longitude": row.longitude
-                }
-            )
+            }
+            for row in result_query
+        ]
         
         # Check if no results were found
         if not trajectories_list:
@@ -67,3 +51,21 @@ def fetch_trajectories(taxi_id, date_initial, date_end):
     finally:
         session.close()
         print("Session closed")
+        
+
+def get_query_with_params_trajectories(result_query, taxi_id, date_initial, date_end):
+    """
+    Get result_query for taxi ID and date from the request.
+    
+    Return:
+        - `taxi_id` (str): The ID of the taxi extracted from the query parameters.
+        - `date_str` (str): The date string extracted from the query parameters.
+    """
+    if taxi_id and date_initial and date_end:
+        return result_query.filter(
+            Trajectories.taxi_id == taxi_id,
+            Trajectories.date >= date_initial,
+            Trajectories.date <= date_end 
+        )
+    return result_query
+    
