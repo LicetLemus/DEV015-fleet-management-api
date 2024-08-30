d742a87bae0ed55152396326146238d490d4d488
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-30 12:07:08 -0500

    chore: improved validation of user-entered date format #10

--- app/controllers/trajectories_controller.py
+++ app/controllers/trajectories_controller.py
@@ -8,10 +8,15 @@ def get_trajectories():
     date = request.args.get('date', type=str)
     
     if not taxi_id or not date:
-        return jsonify({"error": "taxi_id and date are required, Date format must be dd-mm-yyyy"}), 400
+        return jsonify({"error": "taxi_id and date are required"}), 400
 
-    # Convertir la cadena a un objeto datetime
-    date_obj = datetime.strptime(date, '%m-%d-%Y')
+    # Validar el formato de la fecha
+    try:
+        # Intentar convertir la cadena de fecha a un objeto datetime
+        date_obj = datetime.strptime(date, '%d-%m-%Y')
+    except ValueError:
+        # Si falla, el formato de la fecha es incorrecto
+        return jsonify({"error": "Date format must be dd-mm-yyyy"}), 400
     
     # Ajustar la fecha inicial y final para todo el día
     date_initial = date_obj.replace(hour=0, minute=0, second=0, microsecond=0)
