851191c95a613845cebecaaa1e0729fae9e4bcb4
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-09-12 10:10:51 -0500

    doc: writing setting #20

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,23 +3,29 @@ from app import create_app, db
 from app.config import TestingConfig
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 
 @pytest.fixture(scope="module")
 def test_client():
+    # Create an instance of the app with the testing configuration
     app = create_app(TestingConfig)
-    
-    # Configurar la base de datos de pruebas
+
+    # start the app context for initial setup
     with app.app_context():
         # db.drop_all()
-        # Crear tablas nuevas
+        # create the tables in the database for testing
         db.create_all()
 
+    # create a test client to simulate HTTP requests
     with app.test_client() as testing_client:
+        # yield the test client for use in tests
         yield testing_client
 
+    # Reinitialize the app context to clean up the database 
     with app.app_context():
-        # Limpiar la base de datos después de las pruebas
+        # drop all tables after testes are finished
         db.drop_all()
+        # Close any active database sessions
         db.session.remove()
